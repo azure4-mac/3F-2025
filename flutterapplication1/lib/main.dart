7ff555d5f556c32afde3b49267113698f92ee6ba
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2025-04-04 09:34:44 -0300

    Update main.dart

--- flutterapplication1/lib/main.dart
+++ flutterapplication1/lib/main.dart
@@ -1,26 +1,130 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: (ListView(children: [
-        Text("Bom Dia"),
-        Text("Boa Tardes"),
-        Text("Boa Noite"),
-        Text("Bom Dia"),
-        Text("Boa Tardes"),
-        Text("Boa Noite"),
-        Text("Bom Dia"),
-        Text("Boa Tardes"),
-        Text("Boa Noite"),
-        Text("Bom Dia"),
-        Text("Boa Tardes"),
-        Text("Boa Noite"),
-        Text("Bom Dia"),
-        Text("Boa Tardes"),
-        Text("Boa Noite"),
-        Text("Bom Dia"),
-        Text("Boa Tardes"),
-        Text("Boa Noite"),
-      ]))));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TelaInicial()));
+}
+
+class TelaInicial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.ac_unit_outlined))
+        ],
+        backgroundColor: Colors.grey,
+        title: Center(
+            child:
+                Text("Mensagens Bruno", style: TextStyle(color: Colors.black))),
+        leading: Icon(
+          Icons.account_balance_sharp,
+          color: Colors.black,
+          size: 50.1,
+        ),
+      ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[100],
+            child: const Text("He'd have you all unravel at the"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[200],
+            child: const Text('Heed not the rabble'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[300],
+            child: const Text('Sound of screams but the'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[400],
+            child: const Text('Who scream'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[500],
+            child: const Text('Revolution is coming...'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[600],
+            child: const Text('Revolution, they...'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TelaCadastro()),
+          );
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
+
+class TelaCadastro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TelaInicial()));
+                },
+                icon: Icon(Icons.arrow_back_outlined))
+          ],
+          backgroundColor: Colors.grey,
+          title: Center(
+              child: Text("Cantina", style: TextStyle(color: Colors.black))),
+          leading: Icon(
+            Icons.add_shopping_cart_outlined,
+            color: Colors.black,
+            size: 50.1,
+          )),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[100],
+            child: const Text("He'd have you all unravel at the"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[500],
+            child: const Text('Revolution is coming...'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[600],
+            child: const Text('Revolution, they...'),
+          ),
+        ],
+      ),
+    );
+  }
 }
