aed15b600211b50c817982a9341203cb1fac2702
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2025-04-09 16:19:54 -0300

    Update main.dart

--- flutterapplication1/lib/main.dart
+++ flutterapplication1/lib/main.dart
@@ -80,50 +80,64 @@ class TelaInicial extends StatelessWidget {
   }
 }
 
-class TelaCadastro extends StatelessWidget {
+class TelaCadastro extends StatefulWidget {
+  @override
+  _TelaCadastroState createState() => _TelaCadastroState();
+}
+
+class _TelaCadastroState extends State<TelaCadastro> {
+  final List<Map<String, String>> _itens = [];
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _precoController = TextEditingController();
+
+  void _adicionarItem() {
+    setState(() {
+      _itens.add({
+        'nome': _nomeController.text,
+        'preco': _precoController.text,
+      });
+      _nomeController.clear();
+      _precoController.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => TelaInicial()));
+        title: Text('Cadastro de Itens'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nomeController,
+              decoration: InputDecoration(labelText: 'Nome do Item'),
+            ),
+            TextField(
+              controller: _precoController,
+              decoration: InputDecoration(labelText: 'Preço'),
+              keyboardType: TextInputType.number,
+            ),
+            ElevatedButton(
+              onPressed: _adicionarItem,
+              child: Text('Adicionar Item'),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _itens.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_itens[index]['nome']!),
+                    subtitle: Text('Preço: ${_itens[index]['preco']}'),
+                  );
                 },
-                icon: Icon(Icons.arrow_back_outlined))
+              ),
+            ),
           ],
-          backgroundColor: Colors.grey,
-          title: Center(
-              child: Text("Cantina", style: TextStyle(color: Colors.black))),
-          leading: Icon(
-            Icons.add_shopping_cart_outlined,
-            color: Colors.black,
-            size: 50.1,
-          )),
-      body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[100],
-            child: const Text("He'd have you all unravel at the"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[500],
-            child: const Text('Revolution is coming...'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[600],
-            child: const Text('Revolution, they...'),
-          ),
-        ],
+        ),
       ),
     );
   }
